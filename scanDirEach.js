41b3e2534af1cc226f2766d6195634a8c163493c
itonyyo <swzyocowboy@icloud.com>
2021-12-16 17:50:19 +0800

    v1.0.0

---
+++ scanDirEach.js
@@ -0,0 +1,71 @@
+import { readdirSync } from "fs";
+import eachOf from "async/eachOf.js";
+
+import { resolveCwdSync } from "./resolveCwd.js";
+
+export default function scanDirEach(path, worker = () => true, raw = false) {
+  return new Promise((resolve, reject) => {
+    const WORKPIECES = [];
+    const FILES = readdirSync(path, { withFileTypes: true });
+
+    let changed = false;
+
+    function switcher({ wp, pivot }) {
+      switch (Object.prototype.toString.call(wp)) {
+        case "[object Object]":
+          WORKPIECES.push(wp);
+          if (!Object.is(wp, pivot) && !changed) changed = true;
+          break;
+        case "[object Boolean]":
+          if (wp) {
+            WORKPIECES.push(pivot);
+            break;
+          }
+          changed = true;
+          break;
+        default:
+          WORKPIECES.push(pivot);
+          break;
+      }
+    }
+
+    eachOf(
+      FILES,
+      (dirent, index, cb) => {
+        let pivot = {
+          path: resolveCwdSync({ relativePath: dirent.name, cwd: path }),
+          dirent,
+        };
+
+        if (raw) FILES[index] = pivot;
+
+        const MAPPED = worker(pivot, index, FILES);
+
+        if (Object.prototype.toString.call(MAPPED) === "[object Promise]") {
+          MAPPED.then(
+            (tv) => {
+              switcher({ wp: tv, pivot });
+              cb(null);
+            },
+            (err) => {
+              cb(err);
+            }
+          );
+
+          return;
+        }
+
+        switcher({ wp: MAPPED, pivot });
+        cb(null);
+      },
+      (err) => {
+        if (!err) {
+          resolve(raw ? [WORKPIECES, changed ? FILES : false] : WORKPIECES);
+          return;
+        }
+
+        reject(err);
+      }
+    );
+  });
+}
